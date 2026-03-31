25f4dfa1e7ccf7b04cb551d5809f7bf191442005
GrCOTE7 <grcote7@gmail.com>
2026-04-01 01:05:56 +0200

    Nett

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
 from pymox_kit import *
 from datetime import datetime
-
-
-from divers.yt_videos.to_see import (
-    toSeeToBp,
-    videos_to_see,
-    format_remaining_time_fr,
-)
-
-from divers.yt_videos.scrap import (
-    scrap_some
-)
+import flet
 
 if __name__ == "__main__":
 
     cls()
     
     print(title_fr())
-    # print(*["hello" for _ in range(3)], sep="\n")
-
-    # nbmn=2450
-    # print(nbmn, '→', format_remaining_time_fr(nbmn))
-
-    # df  = scrap_some()
-    # print(df)
-
-    # df = videos_to_see()
-    # print()
-
-    # print (len(df), df.shape, df.size)
-
-    # md = toSeeToBp(df)
-    # print(md)
-
-    # display_videos_table(df)
-
+    
+    print(*["hello" for _ in range(3)], sep="\n")
+        
     end()
